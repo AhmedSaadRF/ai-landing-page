42f9af43d8e8c7ca442303153cbe23c168ee830d
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-17 07:15:25 +0300

    Upload Project
    Finish Header Section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,37 @@
+import LogoIcon from "@/assets/logo.svg";
+import MenuIcon from '@/assets/icon-menu.svg'
+
 export const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <div>
+        <div>
+          <div>
+            <div>
+              <LogoIcon />
+            </div>
+          </div>
+          <div>
+            <nav>
+              <a href="#">Features</a>
+              <a href="#">Developers</a>
+              <a href="#">Pricing</a>
+              <a href="#">Changelog</a>
+            </nav>
+          </div>
+          <div>
+            <button>
+              <div>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+              <span>Join waitlist</span>
+            </button>
+            <MenuIcon />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
