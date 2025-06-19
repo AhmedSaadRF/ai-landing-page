d1702b95508b8909678d117ecb3cc7ffbf3b97a8
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-19 22:05:49 +0300

    Upload Project
    Finish Logo Ticker Section Animation

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from 'next/image'
 import acmeLogo from '@/assets/logo-acme.png'
 import apexLogo from '@/assets/logo-apex.png'
@@ -5,6 +7,7 @@ import celestialLogo from '@/assets/logo-celestial.png'
 import quantumLogo from '@/assets/logo-quantum.png'
 import pulseLogo from '@/assets/logo-pulse.png'
 import echoLogo from '@/assets/logo-echo.png'
+import { motion } from 'framer-motion'
 
 export const LogoTicker = () => {
   return (
@@ -14,9 +17,23 @@ export const LogoTicker = () => {
           <div className="flex-1 md:flex-none">
             <h2>Trusted by top innovative teams</h2>
           </div>
-          <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-            <div className="flex flex-none gap-14">
+          <div className="flex flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+            <motion.div
+              initial={{ translateX: '-50%' }}
+              animate={{ translateX: '0' }}
+              transition={{
+                repeat: Infinity,
+                duration: 30,
+                ease: 'linear',
+              }}
+              className="flex flex-none gap-14 pr-14 -translate-x-1/2">
               {[
+                acmeLogo,
+                apexLogo,
+                celestialLogo,
+                quantumLogo,
+                pulseLogo,
+                echoLogo,
                 acmeLogo,
                 apexLogo,
                 celestialLogo,
@@ -32,7 +49,7 @@ export const LogoTicker = () => {
                   height={24}
                 />
               ))}
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
