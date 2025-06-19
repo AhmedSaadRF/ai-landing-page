a79c88b86dfce80e63e455dc9816c2440b903265
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-19 16:25:21 +0300

    Upload Project
    Add Style Classes

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -33,22 +33,22 @@ const testimonials = [
 
 export const Testimonials = () => {
   return (
-    <section>
-      <div>
-        <h2>Beyond Expectations.</h2>
-        <p>Our revolutionary AI SEO tools have transformed our clients strategies.</p>
-        <div>
-          <div>
+    <section className="py-20 md:py-24">
+      <div className="container">
+        <h2 className="text-5xl md:text-6xl text-center tracking-tighter font-medium">Beyond Expectations.</h2>
+        <p className="text-white/70 text-lg md:text-xl text-center mt-5 tracking-tight max-w-sm mx-auto">Our revolutionary AI SEO tools have transformed our clients strategies.</p>
+        <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <div className="flex gap-5">
             {testimonials.map((Testimonial) => (
-              <div key={Testimonial.name}>
-                <div>{Testimonial.text}</div>
-                <div>
-                  <div>
-                    <Image src={Testimonial.avatarImg} alt={`Avatar for ${Testimonial.name}`} />
+              <div key={Testimonial.name} className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none">
+                <div className="text-lg tracking-tight md:text-2xl">{Testimonial.text}</div>
+                <div className="flex items-center gap-3 mt-5">
+                  <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:mix-blend-soft-light before:content-[''] before:absolute before:inset-0 before:border before:border-white/30 before:z-10 before:rounded-lg">
+                    <Image src={Testimonial.avatarImg} alt={`Avatar for ${Testimonial.name}`} className="h-11 w-11 rounded-lg grayscale" />
                   </div>
                   <div>
                     <div>{Testimonial.name}</div>
-                    <div>{Testimonial.title}</div>
+                    <div className="text-white/50 text-sm">{Testimonial.title}</div>
                   </div>
                 </div>
               </div>
