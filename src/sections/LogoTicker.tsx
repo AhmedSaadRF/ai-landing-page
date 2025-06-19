72c8bd75e239656d01a682e9e01f6de28b3fcffc
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-19 12:54:37 +0300

    Upload Project

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,34 @@
+import acmeLogo from '@/assets/logo-acme.png'
+import apexLogo from '@/assets/logo-apex.png'
+import celestialLogo from '@/assets/logo-celestial.png'
+import quantumLogo from '@/assets/logo-quantum.png'
+import pulseLogo from '@/assets/logo-pulse.png'
+import echoLogo from '@/assets/logo-echo.png'
+
 export const LogoTicker = () => {
-  return <section>LogoTicker</section>;
+  return (
+    <section>
+      <div>
+        <div>
+          <div>
+            <h2>Trusted by top innovative teams</h2>
+          </div>
+          <div>
+            <div>
+              {[
+                acmeLogo,
+                apexLogo,
+                celestialLogo,
+                quantumLogo,
+                pulseLogo,
+                echoLogo
+              ].map((logo) => (
+                <img src={logo.src} key={logo.src} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
