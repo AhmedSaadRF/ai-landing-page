02d9c8649316165a9fff682393732e2d6020942b
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-19 18:10:52 +0300

    Upload Project
    Add Style Classes

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -5,24 +5,24 @@ import YTSocial from '@/assets/social-youtube.svg';
 
 export const Footer = () => {
   return (
-    <footer>
-      <div>
-        <div>
-          <div>
-            <Logo />
-            <div>AI Startup Landing Page</div>
+    <footer className='py-5 border-t border-white/15'>
+      <div className='container'>
+        <div className='flex flex-col lg:flex-row items-center gap-8'>
+          <div className='flex gap-2 items-center lg:flex-1'>
+            <Logo className="h-6 w-6" />
+            <div className='font-medium'>AI Startup Landing Page</div>
           </div>
-          <nav>
-            <a href="#">Features</a>
-            <a href="#">Developers</a>
-            <a href="#">Company</a>
-            <a href="#">Blog</a>
-            <a href="#">Changelog</a>
+          <nav className='flex flex-col lg:flex-row gap-5 lg:gap-7 lg:flex-1 lg:justify-center'>
+            <a href="#" className='text-white/70 hover:text-white text-xs md:text-sm transition'>Features</a>
+            <a href="#" className='text-white/70 hover:text-white text-xs md:text-sm transition'>Developers</a>
+            <a href="#" className='text-white/70 hover:text-white text-xs md:text-sm transition'>Company</a>
+            <a href="#" className='text-white/70 hover:text-white text-xs md:text-sm transition'>Blog</a>
+            <a href="#" className='text-white/70 hover:text-white text-xs md:text-sm transition'>Changelog</a>
           </nav>
-          <div>
-            <XSocial />
-            <InstaSocial />
-            <YTSocial />
+          <div className='flex gap-5 lg:flex-1 lg:justify-end'>
+            <XSocial className="text-white/40 hover:text-white transition" />
+            <InstaSocial className="text-white/40 hover:text-white transition" />
+            <YTSocial className="text-white/40 hover:text-white transition" />
           </div>
         </div>
       </div>
