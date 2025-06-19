db3834d2fc4013229616c08728083abbfd36a771
AhmedSaadRF <ahmedsaadalrefaey@gmail.com>
2025-06-20 01:12:41 +0300

    Upload Project

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
 import avatar4 from "@/assets/avatar-4.png";
+import { motion } from "framer-motion";
 import Image from "next/image";
+
 
 const testimonials = [
   {
@@ -37,9 +41,17 @@ export const Testimonials = () => {
       <div className="container">
         <h2 className="text-5xl md:text-6xl text-center tracking-tighter font-medium">Beyond Expectations.</h2>
         <p className="text-white/70 text-lg md:text-xl text-center mt-5 tracking-tight max-w-sm mx-auto">Our revolutionary AI SEO tools have transformed our clients strategies.</p>
-        <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-          <div className="flex gap-5">
-            {testimonials.map((Testimonial) => (
+        <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <motion.div 
+            initial={{ translateX: "-50%" }}
+            animate={{ translateX: "0" }}
+            transition={{
+              repeat: Infinity,
+              ease: "linear",
+              duration: 30,
+            }}
+            className="flex gap-5 pr-5 flex-none -translate-x-1/2">
+            {[...testimonials, ...testimonials].map((Testimonial) => (
               <div key={Testimonial.name} className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none">
                 <div className="text-lg tracking-tight md:text-2xl">{Testimonial.text}</div>
                 <div className="flex items-center gap-3 mt-5">
@@ -53,7 +65,7 @@ export const Testimonials = () => {
                 </div>
               </div>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
