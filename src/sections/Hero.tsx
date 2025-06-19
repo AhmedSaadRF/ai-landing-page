82b5990a5ae1cae3c2e0177525333c9f1decf468
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-19 11:49:07 +0300

    Upload Project
    Finish Hero Section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,43 @@
+import { Button } from "@/components/Button";
+import starBg from "@/assets/stars.png";
+
 export const Hero = () => {
-  return <section>Hero</section>;
+  return (
+    <section>
+      <div></div>
+      {/* Start Planet */}
+      <div></div>
+      {/* End Planet */}
+      {/* Start Ring 1 */}
+      <div>
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+      {/* End Ring 1 */}
+      {/* Start Ring 2 */}
+      <div></div>
+      {/* End Ring 2 */}
+      {/* Start Ring 3 */}
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      {/* End Ring 3 */}
+      <div>
+        <h1>
+          AI SEO
+        </h1>
+        <p>
+          Elevate your site&#39;s visibility effortlessly with AI, where smart
+          technology meets user-friendly SEO tools.
+        </p>
+        <div>
+          <Button>Join waitlist</Button>
+        </div>
+      </div>
+    </section>
+  );
 };
