37692f8c7ccc629ff682f76d7086fa21fe7d56f8
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-19 17:34:33 +0300

    Upload Project
    Finish Call To Action Section

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,30 @@
+import { Button } from "@/components/Button";
+import starsBg from "@/assets/stars.png";
+import gridLines from "@/assets/grid-lines.png";
+
 export const CallToAction = () => {
-  return <section>CallToAction</section>;
+  return (
+    <section>
+      <div>
+        <div
+          style={{
+            backgroundImage: `url(${starsBg.src})`,
+          }}
+        >
+          <div
+            style={{
+              backgroundImage: `url(${gridLines.src})`,
+            }}
+          ></div>
+          <div>
+            <h2>AI-driven SEO for everyone.</h2>
+            <p>Achieve clear, impactful results without the complexity.</p>
+            <div>
+              <Button>Join waitlist</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
